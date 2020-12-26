5d061270d3b99acee82b10a1d30e02309e7f918e
akhilkd <akhil.krsn@gmail.com>
2020-12-26 18:02:03 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# A Python interpreter flask app.
+
+A python interpreter built on flask used to run code your code over a broswer through a subprocess.
+![Screengrab](screenshot.png)
+
+
+## How to use locally
+
+1. Setup your local machine to have Python 3.7, lower versions will not be compatible with APIs in this project.
+
+2. Clone code from the master branch, change to the PyOnline directory. Your machine will need to have the python3-venv package to create virtual environments. It can be installed on Linux Deb distros with :
+> sudo apt-get install python3-venv
+
+3. Create a virtual environment 'env'
+> python3 - venv env
+
+4. Activate the virtual environment
+> source env/bin/activate
+
+5. Ensure all requirements from requirements.txt are satisfied by installing the requrements from the venv.
+
+> pip install -r requirements.txt
+
+6. Run the app from the PyOnline directory
+> python app.py
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import subprocess
+import re
+from constants import appsettings
+ 
+app = Flask(__name__)
+ 
+@app.route('/', methods = ['POST', 'GET'])
+def code():
+    if request.method == 'POST':
+        code = request.form['pycode']
+        f = open("onlinecode.py", "w")
+        f.write(code)
+        f.close()
+        pipe = subprocess.run(["python", "onlinecode.py"], capture_output=True)
+
+        if pipe.returncode == 0:
+            codeoutput = re.sub('[^A-Za-z0-9().,\n\t ]+', '',pipe.stdout.decode('utf-8'))
+            result={"code":code,"codeoutput":codeoutput}
+            return render_template('code.html',result=result)
+        else:
+            codeoutput = re.sub('[^A-Za-z0-9().,\n\t ]+', '',pipe.stderr.decode('utf-8'))
+            return render_template('code.html',result=result)
+
+    if request.method == 'GET':
+        result={"code":"print('Hello World')","codeoutput":"Hello World"}
+        return render_template('code.html',result=result) 
+
+if __name__ == '__main__':
+    app.run(debug=True, host=appsettings.host)
